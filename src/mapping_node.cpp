dfb5d5820ece077c0cfe64b7dbc1dd8a417e60e4
Jianzhu Huai <huai.3@osu.edu>
2023-11-19 20:48:04 +0800

    remove fast_csm_icp localizer

--- src/mapping_node.cpp
+++ src/mapping_node.cpp
@@ -1,34 +1,10 @@
 #include "fastlio/laserMapping.hpp"
-#include <fast_csm_icp/gsm_wrap.h>
 #include <ros/node_handle.h>
 #include <std_msgs/String.h>
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "laserMapping");
   ros::NodeHandle nh;
-  std::string pcdmap_path;
-  nh.param<std::string>("pcdmap", pcdmap_path, "");
-  std::shared_ptr<GSMWrap> gsm(new GSMWrap(nh));
-  std::string fn_path = pcdmap_path.substr(0, pcdmap_path.find_last_of('/')) + "/";
-  gsm->LoadMap(fn_path);
-  ros::Rate rate(50);
-  geometry_msgs::PoseStamped map_T_lidar;
-  while (ros::ok()) {
-    if (gsm->loc_status()) {
-      map_T_lidar = gsm->init_pose();
-      ROS_INFO("Global scan matcher returned initial position %f %f %f", map_T_lidar.pose.position.x, map_T_lidar.pose.position.y, map_T_lidar.pose.position.z);
-      break;
-    }
-    ros::spinOnce();
-    rate.sleep();
-  }
-  gsm.reset(); // remove the gsm node.
-
-  std::vector<double> position{map_T_lidar.pose.position.x, map_T_lidar.pose.position.y, map_T_lidar.pose.position.z};
-  nh.setParam("/mapping/init_world_t_lidar", position);
-  std::vector<double> qxyzw{map_T_lidar.pose.orientation.x, map_T_lidar.pose.orientation.y, map_T_lidar.pose.orientation.z, map_T_lidar.pose.orientation.w};
-  nh.setParam("/mapping/init_world_qxyzw_lidar", qxyzw);
-
   fastlio::LaserMapping node(nh);
   node.spin();
   node.saveAndClose();
