f874881cf59e93bcaf63bc5f8af693e722ce585c
Jianzhu Huai <huai.3@osu.edu>
2024-06-03 10:39:20 +0800

    large scale odom and loc processing

--- config/run_fastlio_loc.sh
+++ config/run_fastlio_loc.sh
@@ -1,47 +1,13 @@
 #!/bin/bash
 
-datadir="/media/$USER/BackupPlus/jhuai/data/homebrew/zongmu"
+
 ws_dir="/media/$USER/docker/lidarslam/fastlio_slam_ws_rel"
 ws_dir="/home/$USER/Documents/lidar/fastlioslam_ws"
-result_dir="/media/$USER/BackupPlus/jhuai/results/fastlio_loc"
+result_dir="/media/$USER/BackupPlus/jhuai/results/front_back_snapshots"
 tls_dir="/media/$USER/BackupPlus/jhuai/data/homebrew/whu_tls_1030"
 init_pose_dir="/media/$USER/BackupPlus/jhuai/results/front_back_snapshots"
 
-bags202312=(20231201/data2
-      20231201/data3
-      20231208/data1
-      20231208/data2
-      20231208/data3
-      20231208/data4
-      20231208/data5
-      20231213/data1
-      20231213/data2
-      20231213/data3
-      20231213/data4
-      20231213/data5)
-
-bags20231105=(
-20231105/data1
-20231105/data2
-20231105/data3
-20231105/data4
-20231105/data5
-20231105/data6
-20231105/data7)
-
-bags20231105_aft=(20231105_aft/data1
-20231105_aft/data2
-20231105_aft/data3
-20231105_aft/data4
-20231105_aft/data5
-20231105_aft/data6)
-
-bags20231109=(20231109/data1
-20231109/data2
-20231109/data3
-20231109/data4)
-
-# The following 3 trajs are covered by and cover the TLS map.
+# The following trajs are covered by and cover the TLS map.
 ref_traj_file1="$ws_dir/src/FAST_LIO/data/20231105_aft/data2/scan_states.txt" # the basketball court loop
 ref_traj_file2="$ws_dir/src/FAST_LIO/data/20231109/data1/scan_states.txt" # the starlake loop
 ref_traj_file3="$ws_dir/src/FAST_LIO/data/20231109/data2/scan_states.txt" # the software school loop
@@ -53,6 +19,7 @@ source devel/setup.bash
 whichend=$1
 echo "whichend: $whichend"
 bagnames=("${@:2}")
+count=0
 for bag in "${bagnames[@]}"; do
   echo "Processing bag: "$bag"_aligned.bag"
   date=${bag%%/*} # the first part of $bag
@@ -79,9 +46,70 @@ for bag in "${bagnames[@]}"; do
       tls_dir:=$tls_dir \
       init_lidar_pose_file:=$init_pose_file \
       save_dir:=$save_dir 2>&1 | tee $save_dir/fastlio.log
+  count=$((count+1))
+  # if [ $count -eq 1 ]; then
+  #   break
+  # fi
 done
 }
 
+datadir="/media/pi/My_Book/jhuai/data/zongmu"
+bags202401=(
+  20240113/data1
+  20240113/data2
+  20240113/data3
+  20240113/data4
+  20240113/data5
+  20240115/data1
+  20240115/data2
+  20240115/data3
+  20240115/data4
+  20240116/data2
+  20240116/data3
+  20240116/data4
+  20240116/data5
+  20240116_eve/data1
+  20240116_eve/data2
+  20240116_eve/data3
+  20240116_eve/data4
+  20240116_eve/data5)
+lidarslam front "${bags202401[@]}"
+lidarslam back "${bags202401[@]}"
 
+datadir="/media/pi/BackupPlus/jhuai/data/homebrew/zongmu"
+bags202312=(20231201/data2
+      20231201/data3
+      20231208/data1
+      20231208/data2
+      20231208/data3
+      20231208/data4
+      20231208/data5
+      20231213/data1
+      20231213/data2
+      20231213/data3
+      20231213/data4
+      20231213/data5)
 lidarslam front "${bags202312[@]}"
 lidarslam back "${bags202312[@]}"
+
+datadir="/media/pi/BackupPlus/jhuai/data/homebrew/ebike"
+bags202311=(
+20231105/data1
+20231105/data2
+20231105/data3
+20231105/data4
+20231105/data5
+20231105/data6
+20231105/data7
+20231105_aft/data1
+20231105_aft/data2
+20231105_aft/data3
+20231105_aft/data4
+20231105_aft/data5
+20231105_aft/data6
+20231109/data1
+20231109/data2
+20231109/data3
+20231109/data4)
+
+lidarslam front "${bags202311[@]}"
